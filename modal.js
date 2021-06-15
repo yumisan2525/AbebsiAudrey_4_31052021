fe99fde8cd3b5dfef7c88dcabca9fd1d1db7b8b2
Yumi San <audreysan87@gmail.com>
2021-06-15 15:03:48 +0200

    fermer la modale

--- modal.js
+++ modal.js
@@ -1,23 +1,32 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+var modalbg = document.querySelector(".bground");
+var modalBtn = document.querySelectorAll(".modal-btn");
+var formData = document.querySelectorAll(".formData");
+var span = document.getElementsByClassName("close")[0];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
-
+// fermer la modale
+span.onclick = function() {
+    modalbg.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modalbg) {
+        modalbg.style.display = "none";
+    }
+}
